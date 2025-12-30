6da30d59daed47d8782a8a649aa674116ac7a09d
Mati Ur Rehman <matiurrehmanmarwatkhan@gmail.com>
2025-12-30 11:12:02 +0500

    Function | Task

---
+++ Javscript/Class_11/jsfuntion.js
@@ -0,0 +1,19 @@
+
+
+function addition (){
+    console.log(2+10);
+    
+}
+
+
+// addition();
+
+function DisplayMassage(){
+    console.log("Welcome to Javascript");
+    
+}
+
+for(let m = 0; m < 10; m++){
+    DisplayMassage();
+}
+// DisplayMassage();
---
+++ Javscript/Class_11/task/script.js
@@ -0,0 +1,46 @@
+// function addition(value1 , value2){
+//     let result = value1 + value2;
+//     alert ("Your Addition Result : " + result) 
+// }
+// function Substraction(value1 , value2){
+//     let result = value1 - value2;
+//     alert ("Your Substraction Result : " + result) 
+// }
+// function Mutiplication(value1 , value2){
+//     let result = value1 * value2;
+//     alert ("Your Multiplication Result : " + result) 
+// }
+
+// addition(Number(prompt("Enter First num : ")),
+//     Number(prompt("Enter Second num : "))
+// )
+
+// Substraction(Number(prompt("Enter First num : ")),
+//     Number(prompt("Enter Second num : "))
+// )
+
+// Mutiplication(Number(prompt("Enter First num : ")),
+//     Number(prompt("Enter Second num : "))
+// )
+
+
+
+
+
+
+// Function Expression
+// let addition = function(){
+    // return 2 + 3    
+// }
+// console.log(addition());
+// addition();
+
+
+// Arrow Function 
+
+// let showName = () => {
+    // console.log("hello World");
+    
+// }
+
+// showName();
