940809e72aef3eda92163c2172a654110106b839
Mati Ur Rehman <matiurrehmanmarwatkhan@gmail.com>
2025-12-31 10:59:04 +0500

    Class_12

---
+++ Javscript/Class_12/jsfunction.js
@@ -0,0 +1,19 @@
+// function addition (){
+//     return 1 + 2 ;
+// }
+// function final(value , add){
+//     console.log(value - add());
+    
+// }
+
+// // final( 5 , addition)
+
+
+
+
+
+// (function ( ){
+//     console.log("Kuch bi");
+// })();
+
+
