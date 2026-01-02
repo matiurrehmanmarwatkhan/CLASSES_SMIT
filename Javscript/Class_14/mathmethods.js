0d19c8eb32520dc1bda126dcaa84fbb6ed761167
Mati Ur Rehman <matiurrehmanmarwatkhan@gmail.com>
2026-01-02 10:47:43 +0500

    Class_14

---
+++ Javscript/Class_14/mathmethods.js
@@ -0,0 +1,27 @@
+// function getrandomInteger(max , min ){
+//     return (Math.round(Math.random() * (max - min)) + min);
+// }
+// console.log(getrandomInteger(20 , 10));
+
+
+
+
+let numbers = Math.max(2,10, 24,43,100,340);
+let numbers1 = Math.min(2,10, 24,43,100,340);
+// console.log(numbers);
+// console.log(numbers1);
+
+
+// console.log(Math.pow(3,2));
+// console.log(Math.sqrt(64));
+
+function power(base,pow){
+    return Math.pow(base , pow)
+}
+
+// console.log(power(3,5));
+// console.log(power(2,5));
+// console.log(power(5,2));
+
+
+
