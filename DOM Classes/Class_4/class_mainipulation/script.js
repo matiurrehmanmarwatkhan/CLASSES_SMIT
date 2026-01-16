5105dcd8a9290b5b5bcb1f163e9bbce9c21383dc
Mati Ur Rehman <matiurrehmanmarwatkhan@gmail.com>
2026-01-16 09:34:16 +0500

    Class_4

---
+++ DOM Classes/Class_4/class_mainipulation/script.js
@@ -0,0 +1,10 @@
+let parentEL= document.getElementById("parentEl");
+
+
+function addClass(){
+    parentEL.classList.add("box")
+}
+
+function removeClass(){
+    parentEL.classList.remove("box")
+}
