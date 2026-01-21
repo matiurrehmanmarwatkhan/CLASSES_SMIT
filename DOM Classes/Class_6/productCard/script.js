73c063736ec7d9324d61c5e21baf03a1fc04bca2
Mati Ur Rehman <matiurrehmanmarwatkhan@gmail.com>
2026-01-21 08:34:05 +0500

    Some task Add

--- DOM Classes/Class_6/productCard/script.js
+++ DOM Classes/Class_6/productCard/script.js
@@ -1,5 +1,6 @@
 // create button click add card
 let button = document.createElement("button")
+
 button.innerText = "Add Cart";
 button.setAttribute("onclick" , "addNewCard()")
 button.setAttribute("class" , "buttones")
@@ -11,22 +12,24 @@ galleryContainer.setAttribute("id" , "gallery")
 document.body.appendChild(galleryContainer)
 
 function addNewCard(){
-    let card = document.createElement("div");
-    let image = document.createElement("img");
-    let h3 = document.createElement("h3");
-    let buyButton = document.createElement("button");
-
-    // image
-    image.setAttribute("src" , "image/Illustration (1).png");
-    image.setAttribute("alt" , "Picture");
-    image.setAttribute("class" , "card-image");
-    card.appendChild(image)
+    for(let i = 1 ; i <= 5 ; i++){
+        
+                let image = document.createElement("img");
+                let card = document.createElement("div");
+                let h3 = document.createElement("h3");
+                let buyButton = document.createElement("button");
+                buyButton.setAttribute("onclick" , "buybuttons()");
+        // image
+        image.setAttribute("src" , "image/Illustration (1).png");
+        image.setAttribute("alt" , "Picture");
+        image.setAttribute("class" , "card-image");
+        card.appendChild(image)
 
 
     // h3
-    h3.innerText = "Card Title";
-    h3.setAttribute("class" , "card-title");
-    card.appendChild(h3);
+        h3.innerText = "Card Title";
+        h3.setAttribute("class" , "card-title");
+        card.appendChild(h3);
 
 
 
@@ -39,4 +42,7 @@ function addNewCard(){
 
     // append card to gallery
     galleryContainer.appendChild(card);
+    }
+    
 }
+    
