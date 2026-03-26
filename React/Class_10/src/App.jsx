6a37db16586fb93d24c5259371491061e03d38de
Mati Ur Rehman <matiurrehmanmarwatkhan@gmail.com>
2026-03-26 10:55:59 +0500

    Changes

---
+++ React/Class_10/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <h2>All Student</h2>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default App;
